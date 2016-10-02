b650e3259182ef4652ea1c67f324e21008ab5001
egunn <erica.gunn@gmail.com>
2016-10-02 17:51:17 -0400

    minor tweaks, page formatting

---
+++ ps-03/lewitt3.js
@@ -0,0 +1,89 @@
+var canvas = document.getElementById('canvas');
+
+var width = canvas.width;
+var height = canvas.height;
+var lineArray;
+var l; //keep track of how many lines have been drawn
+var t; //keep track of how many points (steps along line) to move
+var drawingPad = canvas.getContext('2d');
+//drawingPad.fillStyle = "hsla(0,10%,10%,1)";
+//drawingPad.fillRect(0, 0, 20, 20);
+makeArray();
+
+function makeArray() {
+
+    lineArray = []; //store the ingredients for making lines
+    l = 1; //keep track of how many lines have been drawn
+    t = 1; //keep track of how many points (steps along line) to move
+    drawingPad.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (i = 0; i < 50; i++) {
+        var cc = Math.floor(Math.random() * 255);
+        var x = Math.floor(Math.random()*(width-10));
+        lineArray.push({
+            x1: x,
+            y1: Math.random() * height,
+            x2: x,
+            y2: Math.random() * height,
+            color: "rgb(" + cc + "," + cc + "," + cc + ")"
+        });
+    }
+
+    lineArray.forEach(function (d) {
+        //thanks to http://jsfiddle.net/m1erickson/7faRQ/ for help figuring out the animation of paths
+        // calc waypoints traveling along vertices
+
+        var waypoints = [];
+        var dx = d.x2 - d.x1;
+        var dy = d.y2 - d.y1;
+        for (var j = 0; j < 100; j++) {
+            var x = d.x1 + Math.random()*15 + dx * j / 100;
+            //console.log(x);
+            var y = d.y1 + dy * j / 100;
+            waypoints.push({
+                x: x,
+                y: y
+            });
+        }
+
+        d.waypoints = waypoints;
+
+    });
+
+    // Call the main loop
+    main(0);
+}
+
+// Main loop
+function main(tframe) {
+    // Request animation frames - tells the browser to run this function before the browser refreshes.
+    //Removing this returns a static window
+    //lineArray.forEach(function(d){
+    window.requestAnimationFrame(main);
+
+        test = lineArray[l];
+
+        if (t < test.waypoints.length - 1) {
+
+            drawingPad.beginPath();
+            drawingPad.lineWidth="1.5";
+            drawingPad.strokeStyle = test.color;
+            drawingPad.moveTo(test.waypoints[t - 1].x, test.waypoints[t - 1].y);
+            drawingPad.lineTo(test.waypoints[t].x, test.waypoints[t].y);
+            drawingPad.stroke();
+            t++;
+        }
+
+
+        if(t == 99){
+            l++;
+            t = 1;
+        }
+
+        if(l >= 50){
+            makeArray();
+        }
+
+}
+
+
--- ps-03/squares.js
+++ ps-03/squares.js
@@ -1,4 +1,12 @@
 var canvas = document.getElementById('canvas');
 var drawingPad = canvas.getContext('2d');
 drawingPad.fillStyle = "hsla(0,10%,10%,1)";
-drawingPad.fillRect(0, 0, 20, 20);
+drawingPad.fillRect(0, 0, 70, 70);
+
+
+drawingPad.fillStyle = "hsla(0,10%,10%,.7)";
+drawingPad.fillRect(200, 200, 300, 300);
+
+drawingPad.fillStyle = "hsla(0,10%,10%,0.02)";
+drawingPad.stroke = "black";
+drawingPad.fillRect(120, 120, 450, 450);
